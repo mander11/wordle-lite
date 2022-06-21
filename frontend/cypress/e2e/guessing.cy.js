a7a2b7ecc0f4ddc114ca71a00bc2318e415f7cd3
Matt Anderson <matthew.anderson11@gmail.com>
2022-06-21 08:52:04 -0500

    5 - Adding cypress tests

---
+++ frontend/cypress/e2e/guessing.cy.js
@@ -0,0 +1,29 @@
+describe('happy path', () => {
+  it('success with one guess', () => {
+
+    cy.intercept('POST', '/api/guess', { fixture: 'successful_answer.json'}).as('submitAnswer')
+
+    cy.visit('http://localhost:1234')
+
+    cy.get('input').type('right-answer')
+    cy.get('form').submit()
+
+    cy.wait(['@submitAnswer'])
+
+    cy.get('h3').should('contain', 'Nice!')
+
+  })
+
+  it('failure with one guess', () => {
+    cy.intercept('POST', '/api/guess', { fixture: 'failure_answer.json'}).as('submitAnswer')
+
+    cy.visit('http://localhost:1234')
+
+    cy.get('input').type('wrong-answer')
+    cy.get('form').submit()
+
+    cy.wait(['@submitAnswer'])
+
+    cy.get('h3').should('contain', 'Oh... roasted...  Try again if you dare!')
+  })
+})
