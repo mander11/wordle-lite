57b4bcc020c9e71df858ebd553597be0f5ced642
Matt Anderson <matthew.anderson11@gmail.com>
2022-06-21 08:15:26 -0500

    5 - setting up cypress

--- frontend/cypress/e2e/spec.cy.js
+++ frontend/cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('happy path', () => {
+  it('success with one guess', () => {
+
+    // cy.intercept('POST', '/api/guess', { fixture: 'successful_answer.json'}).as('submitAnswer')
+
     cy.visit('http://localhost:1234')
+
+    // cy.get('input').type('foobar').submit()
+
+    // cy.wait(['@submitAnswer'])
+
+    // cy.get('h3').should('contain', 'Nice!')
+
+
   })
 })
